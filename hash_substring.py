f0194f8e1016331ab04c2c943eafbafa93de85ea
ksnija <125401025+ksnija@users.noreply.github.com>
2023-04-16 18:16:00 +0000

    test1

--- hash_substring.py
+++ hash_substring.py
@@ -1,9 +1,16 @@
 # python3
+# Ksenija Žuka, 18.gr, 221RDC024
 
 def read_input():
     # this function needs to aquire input both from keyboard and file
     # as before, use capital i (input from keyboard) and capital f (input from file) to choose which input type will follow
-    
+    if 'I' in input():
+        pattern = input().strip()
+        text = input().strip()
+    elif 'F' in input():
+        with open("tests/06") as file:
+            pattern = file.readline().rstrip()
+            text = file.readline().rstrip()
     
     # after input type choice
     # read two lines 
@@ -13,7 +20,7 @@ def read_input():
     # return both lines in one return
     
     # this is the sample return, notice the rstrip function
-    return (input().rstrip(), input().rstrip())
+    return (pattern, text)
 
 def print_occurrences(output):
     # this function should control output, it doesn't need any return
@@ -21,9 +28,19 @@ def print_occurrences(output):
 
 def get_occurrences(pattern, text):
     # this function should find the occurances using Rabin Karp alghoritm 
+    patHash = sum(ord(c) for c in pattern) % 101
+    textHash = sum(ord(text[i]) for i in range(len(pattern))) % 101
+    occurances = []
 
+    for i in range(len(text) - len(pattern) + 1):
+        if patHash == textHash:
+            if pattern == text[i:i+len(pattern)]:
+                occurances.append(i)
+        if i<len(text) - len(pattern):
+            textHash = textHash - ord(text[i]) + ord(text[i+len(pattern)])
+            textHash %= 101
     # and return an iterable variable
-    return [0]
+    return occurances
 
 
 # this part launches the functions
